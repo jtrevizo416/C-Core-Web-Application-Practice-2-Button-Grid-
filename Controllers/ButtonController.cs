c400cf7563b120101682d9262705cdb85feb3b5b
jtrevizo416 <jtrevizo416@gmail.com>
2022-06-06 19:17:36 -0500

    Initial Commit

---
+++ Controllers/ButtonController.cs
@@ -0,0 +1,40 @@
+﻿using ButtonGrid.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ButtonGrid.Controllers
+{
+    public class ButtonController : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        Random random = new Random();
+        const int GRID_SIZE = 25;
+        public IActionResult Index()
+        {
+            if (buttons.Count < GRID_SIZE)
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    buttons.Add(new ButtonModel(i, random.Next(4)));
+
+                }
+            }
+            
+
+
+            //buttons.Add(new ButtonModel(4, 2));
+            return View("Index", buttons);
+        }
+
+        public IActionResult HandleButtonClick(string buttonNumber)
+        {
+            //convert from string to int
+            int bn = int.Parse(buttonNumber);
+
+            buttons.ElementAt(bn).ButtonState = (buttons.ElementAt(bn).ButtonState+1) % 4;
+
+            return View("index", buttons);
+        }
+    }
+
+    
+}
---
+++ Models/ButtonModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ButtonGrid.Models
+{
+    public class ButtonModel
+    {
+        public int id;
+        public int buttonState;
+            
+
+        public ButtonModel(int ID, int bState)
+        {
+            id = ID;
+            buttonState = bState;
+        }
+
+        public int ID { get { return id; } set { id = value; } }
+        public int ButtonState { get { return buttonState; } set { buttonState = value; } }
+
+    }
+}
